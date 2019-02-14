bbfd22461e840e8c93a59282539281083f99c1fb
NickleDave <nicholdav@gmail.com>
2019-02-13 19:58:59 -0500

    initial add of src/actviz/

---
+++ src/actviz/fixsim.py
@@ -0,0 +1,154 @@
+"""Simple fixation-based conceptual framework to explain
+reaction times on item-based visual search tasks
+
+Implementation of model proposed in:
+Hulleman, J., & Olivers, C. (2017).
+The impending demise of the item in visual search.
+Behavioral and Brain Sciences, 40, E132.
+doi:10.1017/S0140525X15002794
+"""
+import numpy as np
+
+
+class VisSearchModel:
+    """Model of a subject performing visual search tasks
+
+    """
+    max_items_by_search_type = {
+        'easy': 30,
+        'medium': 7,
+        'hard': 1,
+    }
+
+    def __init__(self,
+                 search_type,
+                 min_items=1,
+                 prev_patch_memory=4,
+                 fixation_duration=250,
+                 quit_threshold=0.85):
+        """__init__
+
+        Parameters
+        ----------
+        search_type : str
+            One of {'easy', 'medium', 'hard'}. Used to determine
+            maximum number of items in functional visual field.
+        min_items : int
+            minimum number of items in functional visual field.
+            Default is 1.
+        prev_patch_memory : int
+            Number of previous patches kept in memory. If a patch is
+            in memory, it will not be revisited when selecting a new
+            patch. Default is 4.
+        fixation_duration : int
+            duration of a fixation in milliseconds. This amount is
+            added to the total trial duration for each fixation.
+        quit_threshold : float
+            between 0 and 1. Percent of search array that has to be
+            seen before quitting search.
+            Default is 0.85, i.e. 85%.
+
+        Notes
+        -----
+        Defaults are as in Hulleman Olivers 2017.
+        """
+        self.search_type = search_type
+        self.min_items = min_items
+        self.max_items = self.max_items_search_type[search_type]
+        self.fvf_vals = np.arange(self.min_items, self.max_items + 1)
+        self.prev_patch_memory = prev_patch_memory
+        self.fixation_duration = fixation_duration
+        self.quit_threshold = quit_threshold
+
+    def _select_new_patch(self, search_arr, fix_locs):
+        """helper function to select a new patch to fixate,
+        given a search array and a list of previous fixation locations
+
+        Parameters
+        ----------
+        search_arr : np.ndarray
+        fix_locs : list
+            of previous fixation locations, i.e., patches. used when
+            selecting new patches. If newly drawn locations are in the
+            set of previous locations kept in memory, i.e. in
+            `fix_locs[-self.prev_patch_memory:]`, then that patch is
+            discarded and another patch is drawn randomly.
+
+        Returns
+        -------
+        fix_loc : int
+            index of new fixation location, i.e. "patch"
+        """
+        fix_loc = -1
+        while fix_loc == -1:
+            fix_loc_tmp = np.random.choice(np.arange(len(search_arr) - 1))
+            if fix_loc_tmp not in fix_locs[-self.prev_patch_memory:]:
+                fix_loc = fix_loc_tmp
+        return fix_loc
+
+    def _get_fvf_size(self):
+        """helper function to get size of functional visual field for
+        a fixation.
+
+        Returns
+        -------
+        fvf_size : int
+            uniform draw from self.min_items to self.max_items inclusive,
+            using numpy.random.choice on self.fvf_vals
+        """
+        return np.random.choice(self.fvf_vals)  # uses uniform probability
+
+    def _fixate(self, search_arr, target, fix_loc, fvf_size, seen_arr):
+        """helper function that simulates fixation
+
+        Parameters
+        ----------
+        search_arr : numpy.ndarray
+        target : int
+        fix_loc : int
+            index of fixation location, i.e. where it starts
+        fvf_size : int
+            size of functional field of view.
+        seen_arr : numpy.ndarray
+            Elements in search_arr that are part of a fixation are
+            converted to True in this array. Used to determine whether
+            quit_threshold has been passed
+
+        Returns
+        -------
+        target_present : bool
+            True if target in functional visual field that is found by
+            "fixating" (indexing into search_arr). False otherwise.
+
+        Notes
+        -----
+        As currently implemented, the functional visual field starts at
+        fix_loc and ends at fix_loc + fvf_size. If this index goes beyond
+        the end of the array, the fvf is simply truncated.
+        """
+        fvf = search_arr[fix_loc:fix_loc + fvf_size]
+        seen_arr[fix_loc:fix_loc + fvf_size] = True
+        if target in fvf:
+            return True, seen_arr
+        else:
+            return False, seen_arr
+
+    def run_trial(self, search_arr, target):
+        """run a single trial of visual search task
+        """
+        responded = False
+        reaction_time = 0
+        fix_locs = []  # locations of fixations
+        seen_arr = np.zeros(search_arr.shape)
+        while responded is False:
+            fix_loc = self._select_new_patch(search_arr, fix_locs)
+            fvf_size = self._get_fvf_size()
+            target_present, seen_arr = self._fixate(search_arr,
+                                                    target,
+                                                    fix_loc,
+                                                    fvf_size,
+                                                    seen_arr)
+            reaction_time += self.fixation_duration
+            if target_present or (np.sum(seen_arr) / seen_arr.shape > self.quit_threshold):
+                responded = True
+        return target_present, reaction_time
