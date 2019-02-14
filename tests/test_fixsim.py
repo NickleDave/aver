a6e10a0cd8832110df1a4ddb72565b2f758d471e
NickleDave <nicholdav@gmail.com>
2019-02-13 20:53:48 -0500

    add tests/test_fixsim.py

---
+++ tests/test_fixsim.py
@@ -0,0 +1,15 @@
+import unittest
+
+import actviz
+
+
+class TestFixSim(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
