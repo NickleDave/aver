5b8df2e5b0dd9a102ae0dafc7494c2476c21e7bc
NickleDave <nickledave@users.noreply.github.com>
2019-02-14 18:50:37 -0500

    __main__ executes Simulator.runall() and saves results in .json and .pickle files

--- src/fvf/__main__.py
+++ src/fvf/__main__.py
@@ -1,4 +1,8 @@
 import argparse
+import pickle
+import json
+
+import numpy as np
 
 from .simulator import Simulator
 
@@ -12,7 +16,18 @@ def main():
     parser = get_parser()
     parser.parse_args()
     sim = Simulator()
-    sim.run()
+    results = sim.runall()
+
+    with open('./results.pickle', 'wb') as fp:
+        pickle.dump(results, fp)
+
+    reaction_times = {}
+    for (search_type, display_size, target_present), trials in results.items():
+        RTs = [trial.reaction_time for trial in trials]
+        key = ', '.join([search_type, str(display_size), str(target_present)])
+        reaction_times[key] = RTs
+    with open('./reaction_times.json', 'w') as fp:
+        json.dump(reaction_times, fp)
 
 
 if __name__ == '__main__':
