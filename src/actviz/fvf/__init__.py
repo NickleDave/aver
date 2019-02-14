a8bfc217117803267e97bc552dcde3fb536d8b2c
NickleDave <nicholdav@gmail.com>
2019-02-13 21:13:34 -0500

    add src/actviz/fvf/__init__.py

---
+++ src/actviz/fvf/__init__.py
@@ -0,0 +1,10 @@
+"""
+fvf: simple fixation-based conceptual framework to explain
+reaction times on item-based visual search tasks
+
+Implementation of framework proposed in:
+Hulleman, J., & Olivers, C. (2017).
+The impending demise of the item in visual search.
+Behavioral and Brain Sciences, 40, E132.
+doi:10.1017/S0140525X15002794
+"""
