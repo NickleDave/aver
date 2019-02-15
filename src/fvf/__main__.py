6426eaf052d5acd40cd670fecec30d0e2596debf
NickleDave <nickledave@users.noreply.github.com>
2019-02-15 10:46:14 -0500

    add cli args results_dir, loglevel, and seed to fvf/__main__.py

--- src/fvf/__main__.py
+++ src/fvf/__main__.py
@@ -1,32 +1,53 @@
 import argparse
 import pickle
 import json
+import os
+import logging
 
-import numpy as np
 
 from .simulator import Simulator
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Run simulations of fixation-based framework.')
+    parser.add_argument('results_dir', type=str, help='name of directory where results should be saved')
+    parser.add_argument('loglevel', nargs='?', default='INFO', choices=('INFO', 'DEBUG', 'WARNING'),
+                        help="logging level (defaults to logging.INFO)")
+    parser.add_argument('seed', nargs='?', default=42, type=int,
+                        help="seed for numpy random generator, default is 42")
     return parser
 
 
 def main():
     parser = get_parser()
-    parser.parse_args()
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.loglevel))
+    logger = logging.getLogger(__name__)
+
+    if not os.path.isdir(args.results_dir):
+        logger.info(f'making directory {args.results_dir}')
+        os.makedirs(args.results_dir)
+
+    logger.info('starting simulation')
     sim = Simulator()
     results = sim.runall()
 
-    with open('./results.pickle', 'wb') as fp:
+    results_pkl = os.path.join(args.results_dir, 'results.pickle')
+    logger.info(f'saving results in {results_pkl}')
+    with open(results_pkl, 'wb') as fp:
         pickle.dump(results, fp)
 
+    # munge reaction times
+    logger.info('getting just reaction times out of {results_pkl}')
     reaction_times = {}
     for (search_type, display_size, target_present), trials in results.items():
         RTs = [trial.reaction_time for trial in trials]
         key = ', '.join([search_type, str(display_size), str(target_present)])
         reaction_times[key] = RTs
-    with open('./reaction_times.json', 'w') as fp:
+    rt_json = os.path.join(args.results_dir, 'reaction_times.json')
+    logger.info(f'saving reaction times in {rt_json}')
+    with open(rt_json, 'w') as fp:
         json.dump(reaction_times, fp)
 
 
